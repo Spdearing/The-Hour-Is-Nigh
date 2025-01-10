bbb2dab7f7d3e2d8b52e0d977fdb4643a71cb3dc
Spdearing <142638816+Spdearing@users.noreply.github.com>
2025-01-10 17:23:43 -0500

    Raycast works

--- The_Hour_Is_Nigh/Assets/Scripts/Camera/CameraRaycast.cs
+++ The_Hour_Is_Nigh/Assets/Scripts/Camera/CameraRaycast.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class CameraRaycast : MonoBehaviour
 {
+    [Header("Game Objects")]
+    [SerializeField] private GameObject lastObjectHit;
+
+    [Header("Floats")]
+    [SerializeField] private float interactDistance;
+    [SerializeField] private float raycastDistance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,21 @@ public class CameraRaycast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, raycastDistance))
+        {
+            string objectHitTag = hitInfo.collider.gameObject.tag;
+
+            GameObject objectHit = hitInfo.collider.gameObject;
+
+            Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.red);
+
+            if (hitInfo.distance < interactDistance)
+            {
+                Debug.Log(objectHit.name);
+            }
+        }
     }
 }
