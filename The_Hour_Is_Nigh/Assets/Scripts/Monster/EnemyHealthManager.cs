6b59ace02a7e2b7b48b05ec4206d92687ed4ddab
Spdearing <142638816+Spdearing@users.noreply.github.com>
2025-01-20 20:49:31 -0500

    small changes

--- The_Hour_Is_Nigh/Assets/Scripts/Monster/EnemyHealthManager.cs
+++ The_Hour_Is_Nigh/Assets/Scripts/Monster/EnemyHealthManager.cs
@@ -15,10 +15,6 @@ public class EnemyHealthManager : MonoBehaviour
     [Header("Scripts")]
     [SerializeField] BlobBehavior blobBehavior;
 
-
-
-
-
     // Start is called before the first frame update
     void Start()
     {
