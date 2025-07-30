ea099dff92fe06e99016c7bb8bd240ec6c53650a
Sammy P <sdearing0409@outlook.com>
2025-07-29 20:29:33 -0400

    working on getting the card objects to be set, when the cards are delt

--- The_Hour_Is_Nigh/Assets/Resources/Scripts/BlackJack/Card.cs
+++ The_Hour_Is_Nigh/Assets/Resources/Scripts/BlackJack/Card.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "Card", menuName = "Card")]
 
 public class Card : ScriptableObject
 {
+
+    [SerializeField] private Dictionary<string, Image> deckImages = new Dictionary<string, Image>();
+
+
     [SerializeField] private string suit;
     [SerializeField] private int value;
+    [SerializeField] private Image suitImage;
     [SerializeField] private GameObject card;
 
     public string GetSuit()
@@ -19,6 +25,11 @@ public class Card : ScriptableObject
     {
         return this.value;
     }
+
+    public void SetSuitImage(string suit)
+    {
+            
+    }
 }
 
-        
+
