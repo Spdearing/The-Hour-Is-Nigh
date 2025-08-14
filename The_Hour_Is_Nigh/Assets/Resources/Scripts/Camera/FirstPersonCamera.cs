bc047d93be51c10581d42e1368a3743ff159eeac
Ryan Moore <ry.moore2003@gmail.com>
2025-08-14 17:44:39 -0400

    Add new camera and player movement scripts, update scene and project settings

---
+++ The_Hour_Is_Nigh/Assets/Resources/Scripts/Camera/FirstPersonCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public float lookspeed;
+    public GameObject player;
+
+    private float _yaw;
+    private float _pitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _yaw += lookspeed * Input.GetAxis("Mouse X");
+        _pitch -= lookspeed * Input.GetAxis("Mouse Y");
+
+        _pitch = Mathf.Clamp(_pitch, -90f, 90f);
+
+        transform.eulerAngles = new Vector3(_pitch, _yaw, 0);
+
+        player.GetComponent<Transform>().rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+
+        //camera stays with player but is not child of player
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + .6f, player.transform.position.z);
+    }
+}
